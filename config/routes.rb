df3e17d7ed74ffc2b5531075d50aad30edd64b16
Germaine Mojica <germaine.mojica@gmail.com>
2022-03-04 16:38:01 +0800

    Add route for devise user. Remove tasks#index in the nested routes and add a standalone route for it

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   root "categories#index"
-  
+
+  devise_for :users
+
+  get "tasks", to: "tasks#index"
+
   resources :categories do
-    resources :tasks
+    resources :tasks, except: :index
   end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
